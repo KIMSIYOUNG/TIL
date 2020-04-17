98eee545e0eeecd6c051b933183859ff97b321c6
KIMSIYOUNG <tldud2404@gmail.com>
2020-04-17 20:06:58 +0900

    Springboot : Add basic eventPulisher example

--- boot/src/main/java/com/spring/boot/BootApplication.java
+++ boot/src/main/java/com/spring/boot/BootApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootApplication.class, args);
+		SpringApplication app = new SpringApplication(BootApplication.class);
+		app.run(args);
 	}
 
 }
+
