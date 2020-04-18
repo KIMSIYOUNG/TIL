8164cb382cfe0c4133a8df366046fe26581c4c50
KIMSIYOUNG <tldud2404@gmail.com>
2020-04-18 13:14:55 +0900

    Springboot : Add H2 example

--- boot/src/main/java/com/spring/boot/BootApplication.java
+++ boot/src/main/java/com/spring/boot/BootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(BootApplication.class);
 		app.run(args);
